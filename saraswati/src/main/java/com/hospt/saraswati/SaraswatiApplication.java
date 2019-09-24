56049fd5f63ec0ea7719785ca70bac81ea69ef04
ashim anand <ashimanand@ashims-MacBook-Air.local>
2019-09-24 16:30:33 +0530

    Adding project structure

---
+++ saraswati/src/main/java/com/hospt/saraswati/SaraswatiApplication.java
@@ -0,0 +1,13 @@
+package com.hospt.saraswati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraswatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaraswatiApplication.class, args);
+	}
+
+}
---
+++ saraswati/src/test/java/com/hospt/saraswati/SaraswatiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hospt.saraswati;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaraswatiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
